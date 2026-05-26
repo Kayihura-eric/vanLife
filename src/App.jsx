25c022e44f39a9419238617a4cb9c5e108dd0cb2
eric <erickayihura900@gmail.com>
2026-05-26 07:58:07 +0200

    Van detail where you pick one van

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 import Home from './components/Home';
 import About from './components/About';
 import Vans from './components/Vans';
+import VanDetail from "./components/VanDetail";
 import "./App.css";
 import './Server'
 export default function App() {
@@ -27,6 +28,8 @@ export default function App() {
         <Route exact path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
         <Route path = "/vans" element = {<Vans />} />
+        <Route path="/vans/:id" element = {<VanDetail />} />
+
       </Routes>
     </BrowserRouter>
   );
